e3cb5f1eee7c337cd37c3719f0cd68174a4cd1bd
ifeslopes <ifes.lopes@gmail.com>
2023-03-22 11:38:08 -0300

    Inicio

---
+++ src/model/Produto.java
@@ -0,0 +1,56 @@
+package model;
+
+public class Produto {
+
+    private int codigo;
+    private String nome;
+    private int quantidade;
+    private double precoDeVenda;
+    private static int geradorDeCodigo =1;
+
+    public Produto(String nome, int quantidade, double precoDeVenda) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.precoDeVenda = precoDeVenda;
+        this.codigo = geradorDeCodigo;
+        geradorDeCodigo++;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPrecoDeVenda() {
+        return precoDeVenda;
+    }
+
+    public void setPrecoDeVenda(double precoDeVenda) {
+        this.precoDeVenda = precoDeVenda;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "codigo=" + codigo +
+                ", nome='" + nome + '\'' +
+                ", quantidade=" + quantidade +
+                ", precoDeVenda=" + precoDeVenda +
+                '}';
+    }
+}
